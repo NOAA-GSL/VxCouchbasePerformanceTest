af4ea022d93f2a87d54e0bc4c5ecfbe035b4a5a1
Randy Pierce <pierce@adb-cb1.gsd.esrl.noaa.gov>
2020-05-27 14:23:45 +0000

    added load spec para

--- test/bin/run_mvload_mysql.sh
+++ test/bin/run_mvload_mysql.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+load_spec=$1
+if [ -f "$load_spec" ]; then
+    echo "loading $load_spec"
+else 
+    echo "$load_spec does not exist - must exit"
+    exit 1
 export MV_HOME=/home/pierce/METViewer
 export JAVA_HOME=/home/pierce/jdk-14.0.1
 export PATH=$JAVA_HOME/bin:$PATH
-time  ${MV_HOME}/bin/mv_load.sh /home/pierce/CouchbasePerformanceTest/test/mv_gfs_grid2obs_vsdb1-mysql.xml 
+time  ${MV_HOME}/bin/mv_load.sh $load_spec
