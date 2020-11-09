a284fee6863f18d631b6b515c74174d77fe3c246
Randy Pierce <pierce@adb-cb1.gsd.esrl.noaa.gov>
2020-11-09 19:39:32 +0000

    work in progress

--- test/test_cases/run_scenarios.sh
+++ test/test_cases/run_scenarios.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 d=$(date +%Y%m%d:%T)-vsdb
+echo $d
 ./run_all_tests.sh mv_gfs_grid2obs_vsdb > ${d}.out
 mv ${d}.out output       
 cp -a output ${d}-output
 
 d=$(date +%Y%m%d:%T)-vsdb1
+echo $d
 ./run_all_tests.sh mv_gfs_grid2obs_vsdb1 > ${d}.out
 mv ${d}.out output       
 cp -a output ${d}-output
 
 d=$(date +%Y%m%d:%T)-vsdb2
+echo $d
 ./run_all_tests.sh mv_gfs_grid2obs_vsdb2 > ${d}.out
 mv ${d}.out output       
 cp -a output ${d}-output
